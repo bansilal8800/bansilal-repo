4aeb3327cd578cefbf1a3aaf2634d22c6381a37a
bansilal8800 <bansilal8800@gmail.com>
2025-01-30 13:05:03 +0530

    added a folder

---
+++ Python-Practice/best_practice.py
@@ -0,0 +1,17 @@
+my_list =[1,1,3,4]
+my_list=[1]
+print(my_list)
+
+
+my_dict ={'ItemId': 123 , 'ItemName': "Banana", "Quantity": 4, "Racks": [{'ItemId': 123 , 'ItemName': "Banana", "Quantity": 4, "Racks": ["u-1","u2","u-3","u-4"]},"u2","u-3","u-4"]}
+
+list_dict =[1,'Bansilal',{'Height':162,'Weight':63,'Age':31,'Colleges':['RGUKT',{'APIIT':'Autonomous',"APIIT": 'Afliated'},'IIT']}]
+print(my_dict["Racks"])
+print(my_dict["Racks"][0]['Racks'])
+print(my_dict["Racks"][1])
+print(list_dict[2]['Age'])
+print(list_dict[2]['Colleges'][1]['APIIT'])
+
+
+
+
